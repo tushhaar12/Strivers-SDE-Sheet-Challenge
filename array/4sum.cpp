8a1334e93afa0ce56004e30109dd43c9a6c5b93b
tushhaar12 <106998128+tushhaar12@users.noreply.github.com>
2022-06-08 19:54:16 +0530

    Created 4sum.cpp inside array folder

---
+++ array/4sum.cpp
@@ -0,0 +1,57 @@
+
+vector<vector<int>> fourSum(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    if (n == 0)
+        return ans;
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int target2 = target - nums[i] - nums[j];
+
+            int front = j + 1;
+            int back = n - 1;
+
+            while (front < back)
+            {
+                int two_sum = nums[front] + nums[back];
+
+                if (two_sum > target2)
+                {
+                    back--;
+                }
+                else if (two_sum < target2)
+                {
+                    front++;
+                }
+                else
+                {
+                    vector<int> quad(4, 0);
+                    quad[0] = nums[i];
+                    quad[1] = nums[j];
+                    quad[2] = nums[front];
+                    quad[3] = nums[back];
+
+                    ans.push_back(quad);
+
+                    while (front < back && nums[front] == quad[2])
+                        front++;
+                    while (front < back && nums[back] == quad[3])
+                        back--;
+                }
+            }
+
+            while (j + 1 < n && nums[j + 1] == nums[j])
+                j++;
+        }
+        while (i + 1 < n && nums[i + 1] == nums[i])
+            i++;
+    }
+    return ans;
+}
