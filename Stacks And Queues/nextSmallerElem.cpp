7de586d8ed07467b7b5a0fe4cf995408fc492a09
tushhaar12 <106998128+tushhaar12@users.noreply.github.com>
2022-07-21 19:53:19 +0530

    Created nextSmallerElem.cpp file.

---
+++ Stacks And Queues/nextSmallerElem.cpp
@@ -0,0 +1,15 @@
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    vector<int> ans(n, -1);
+    stack<int> st;
+    st.push(-1);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && st.top() >= arr[i])
+            st.pop();
+        ans[i] = st.top();
+        st.push(arr[i]);
+    }
+    return ans;
+}
